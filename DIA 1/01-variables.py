893b83cd6a8b4789a091406bd5c0fef6659e04b9
Joshua Arnao Canessa <joshuaarnaocanessa@Joshuas-MacBook-M3.local>
2025-06-06 15:10:36 -0500

    first commit # Working directory # git add . solo agrega los archivos modificados y agregados del nivel actual o novel inferiro # git add -A agregará TODOS los archivos que pertenezcan al repositorio # U: Untracked(Sin seguimiento)
    # ↓
    # Stanting area
    # A: Addesd(agregados al staging area)
    
    # ↓
    # git directory

---
+++ DIA 1/01-variables.py
@@ -0,0 +1,28 @@
+# Variables: un lugar donde se almacena un contenido
+# Todas las variables pueden cambiar sus valores
+
+# Variables de texto
+name = 'Joshua'
+lastname = 'Arnao'
+
+# Texto con saltos de línea (STR)
+description = '''
+Hello:
+How are u?
+'''
+
+# Variables númericas (INT)
+age = 30
+year = 2025
+
+# Variable Vacía None = null || undefined (NoneType)
+newType = None
+
+# id() -> Dará una ubicación de esa variable en relación a la memoría del dispositivo
+
+# Declaración de variables en una misma línea
+month, day = 'Febrery', 28
+
+
+# del -> Elimina la variable de la memoria
+del month
