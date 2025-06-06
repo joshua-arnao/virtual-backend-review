b3d772c2b265253dec5ccedd4ee735727f590fa5
Joshua Arnao Canessa <joshuaarnaocanessa@Joshuas-MacBook-M3.local>
2025-06-06 16:25:00 -0500

    finish review python operators

---
+++ DIA 1/03-operadores-aritmeticos.py
@@ -0,0 +1,20 @@
+number1 = 10
+number2 = 20
+
+# SUM
+print(number1 + number2)
+
+# SUBTRACTION
+print(number1 - number2)
+
+# MULTIPLICATION
+print(number1 * number2)
+
+# DIVISION
+print(number1 / number2)
+
+# MODULE
+print(number1 % number2)
+
+# QUOTIENT
+print(number1 // number2)
